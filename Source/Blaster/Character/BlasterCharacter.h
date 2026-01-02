0e9623b1be01d5f7f1102e5b7539476c0482bc99
Larry_Lin <3213573967@qq.com>
2026-01-02 09:33:44 +0800

    Character Camera and Spring Arm

---
+++ Source/Blaster/Character/BlasterCharacter.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "BlasterCharacter.generated.h"
+
+class UCameraComponent;
+class USpringArmComponent;
+
+UCLASS()
+class BLASTER_API ABlasterCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ABlasterCharacter();
+
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(VisibleAnywhere , Category = "Camera")
+	USpringArmComponent* CameraBoom;
+
+	UPROPERTY(visibleAnywhere, Category = "Camera")
+	UCameraComponent* FollowCamera;
+};
